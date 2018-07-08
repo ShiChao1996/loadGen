3cec97e62395aebbc90240833543eba08d351a0f
ShiChao1996 <2483061998@qq.com>
2018-07-08 21:35:00 +0800

    Initial commit

---
+++ lib/interface.go
@@ -0,0 +1,83 @@
+package lib
+
+import "time"
+
+
+const (
+	STATUS_ORIGIN    uint32 = iota
+	STATUS_STARTTING
+	STATUS_STARTTED
+	STATUS_STOPPED
+)
+
+type LoadGenerator interface {
+	// Start start to generator load
+	Start() bool
+
+	// Stop stops the generator
+	Stop() bool
+
+	Status() uint32
+
+	// CallCount returns number of calls
+	CallCount() uint32
+}
+
+type Caller interface {
+	BuildReq() RawReq
+
+	Call(req []byte) ([]byte, error)
+
+	CheckResp(req RawReq, resp RawResp) bool
+}
+/*
+type RawReq interface {
+	ID() uint64
+
+	// Bytes marshals RawReq to []byte to send
+	Bytes() []byte
+}
+
+type RawResp interface {
+	ID() uint64
+
+	// Bytes marshals RawResp to []byte
+	Bytes() []byte
+
+	Err() error
+
+	// Elapse returns time duration that the server used to handle request
+	Elapse() time.Duration
+}
+*/
+type Tickets interface {
+	Init()
+	Put()
+	Get()
+	Active() bool
+	Total() uint32
+	Remainder() uint32
+}
+
+
+type Result struct {
+	ID     int32
+	Req    RawReq
+	Resp   RawResp
+	Code   int32 // retcode
+	Msg    string
+	Elapse time.Duration
+}
+
+type RawReq struct {
+	id  int32
+	req []byte
+}
+
+
+type RawResp struct {
+	id  int32
+	res []byte
+	err error
+}
+
